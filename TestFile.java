08c0c1765054ffefe89e6d3559dcfa97883f0400
shaiksha <shaiknaveed5811@gmail.com>
2024-09-13 19:26:19 +0530

    Test cases

--- TestFile.java
+++ TestFile.java
@@ -1,36 +1,47 @@
 package User_registration;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class TestFile {
       @Test
 	 public void test1() {
     	  TestMethods t =new TestMethods();
-    	  t.Testfname("Shaik");
+    	boolean s=  t.Testfname("Shaik");
+    	assertTrue(s);
 	 }
       
       @Test
  	 public void test2() {
     	  TestMethods t =new TestMethods();
-          t.Testlname("Vali");
+    	  boolean s= t.Testlname("Vali");
+      	assertTrue(s);
+
  	 }
       
       @Test
 	 public void test3() {
     	  TestMethods t =new TestMethods();
-           t.Testmail("shaik123@gmail.com");
+    	  boolean s=  t.Testmail("shaik123@gmail.com");
+      	assertTrue(s);
+
 	 }
       
       @Test
 	 public void test4() {
     	  TestMethods t =new TestMethods();
-            t.Testnum("6303833290");
+    	  boolean s=   t.Testnum("6303833290");
+      	assertTrue(s);
+
 	 }
       
       @Test
 	 public void test5() {
     	  TestMethods t =new TestMethods();
-          t.Testpass("Shaik@13");
+    	  boolean s=  t.Testpass("Shaik@13");
+      	assertTrue(s);
+
 	 }
        
 }
